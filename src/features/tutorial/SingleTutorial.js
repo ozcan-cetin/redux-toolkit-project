5362f7208299e13e88edb465acb7af75e4697d1b
özcan çetin <cetin445@gmail.com>
2022-09-14 00:39:32 +0300

    v1

--- src/features/tutorial/SingleTutorial.js
+++ src/features/tutorial/SingleTutorial.js
@@ -1,20 +1,15 @@
 import React from 'react'
-import {BiEdit} from "react-icons/bi";
-import {FaTrashAlt} from "react-icons/fa";
-import {TiArrowBack} from "react-icons/ti";
+import { useNavigate } from 'react-router-dom';
 
 const SingleTutorial = ({item}) => {
+  const navigate = useNavigate()
     const {id,title,content}=item;
   return (
     <>
         <div className="card ">
             <h4 className="card-title">{title}</h4>
             <p className="card-content">{content}</p>
-            <div className='btn-div'>
-                <button><TiArrowBack/></button>
-                <button><BiEdit/></button>
-                <button><FaTrashAlt/></button>
-            </div>
+            <button onClick={()=>navigate("/detail", {state:item})}>More</button>
         </div>
     </>
   )
--- src/features/tutorial/TutorialDetail.js
+++ src/features/tutorial/TutorialDetail.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import {BiEdit} from "react-icons/bi";
+import {FaTrashAlt} from "react-icons/fa";
+import {TiArrowBack} from "react-icons/ti";
+import { useLocation, useNavigate } from 'react-router-dom';
+import {formatDistanceToNow, parseISO} from "date-fns"
+import { useDispatch } from 'react-redux';
+import { removeTutorial } from './tutorialSlice';
+
 
 const TutorialDetail = () => {
+  const dispatch=useDispatch()
+  const {state}=useLocation()
+  const{id, title, content, date} = state
+  const fromThenToNow=parseISO(date) 
+  const navigate = useNavigate()
+
+  const deleteItem = () => {
+    dispatch(removeTutorial(id))
+    navigate("/")
+  }
   return (
-    <div>TutorialDetail</div>
+    <div>
+        <h4 className="card-title">{title}</h4>
+            <p className="card-content">{content}</p>
+            <p>{formatDistanceToNow(fromThenToNow)} ago</p>
+            <button onClick={()=>navigate(-1)}><TiArrowBack/></button>
+                <button><BiEdit/></button>
+                <button onClick={deleteItem}><FaTrashAlt/></button>
+    </div>
   )
 }
 
